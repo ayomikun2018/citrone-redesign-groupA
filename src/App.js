bc855e0bd7b81960f3dbef9ce9503bf1437e50be
ayomikun2018 <ayomikunesther12@gmail.com>
2023-03-20 16:34:08 +0100

    created-react-app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {Route, Routes} from 'react-router';
+import LandingPage from "./components/Pages/LandingPage";
+import Signup from "./components/Pages/Signup";
+import Login from "./components/Pages/Login";
+import Dashboard from "./components/Pages/Dashboard";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+   <Routes>
+   <Route exact path= '/' element = {<LandingPage/>}/>
+   <Route exact path= '/signup' element = {<Signup/>}/>
+   <Route exact path= '/login' element = {<Login/>}/>
+   <Route exact path= '/dashboard' element = {<Dashboard/>}/>
+   </Routes>
   );
 }
 
