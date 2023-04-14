66d5b37c9527dfecc99055f18636588db865d087
Bookieblue <oluwabukola.fasan@gmail.com>
2023-04-14 18:39:58 +0100

    authentication

--- src/components/Pages/Courses/Courses.js
+++ src/components/Pages/Courses/Courses.js
@@ -23,7 +23,7 @@ const Courses = () => {
       <div class="w-100 h-sceen absolute my-48 ml-72">
          <div> 
          <p class="text-lg mb-3 ">Continue from where you stopped</p>
-          <img src={computer}></img>
+          <img class=' h-64 w-4/5' src={computer}></img>
           <p class="absolute top-16 text-white text-base ml-6 font-meduim">Module 10: Responsive Design</p>
         </div>
         <input type='text' placeholder='Search' class='absolute h-12 w-80 top-5 -right-1/2 mr-2  pl-14 border border-grey rounded-xl '></input>
@@ -35,41 +35,41 @@ const Courses = () => {
         <h1 class="text-2xl font-extrabold mt-8">Course Content</h1>
         <p class="text-base font-extrabold text-gray-500 mt-3">UI/UX Intermediate Level</p>
         <div class="grid grid-cols-3 pt-4">
-          <img src={DesignBook} alt='Designbook'></img>
-          <img src={Notes}  alt='Notes' class="pl-3"></img>
-          <img src={Groupwork} alt='groupdesign' class="pl-6"></img>
-          <p>Module 1: Introduction to<p>design</p></p>
-          <p class="pl-3">Module 2: Sketching</p>
-          <p class="pl-6">Module 3: Ideation</p>
+          <img src={DesignBook} class=" h-32 w-64" alt='Designbook'></img>
+          <img src={Notes} alt='Notes' class="pl-3  h-32 w-64"></img>
+          <img src={Groupwork}  alt='groupdesign' class="pl-3  h-32 w-64"></img>
+          <p class="text-sm pt-2 font-bold">Module 1: Introduction to<p class="text-sm font-bold">design</p></p>
+          <p class="pl-3 text-sm pt-2 font-bold">Module 2: Sketching</p>
+          <p class="pl-4 text-sm pt-2 font-bold">Module 3: Ideation</p>
         </div>
         <h1 class="text-2xl font-extrabold mt-8">Recorded Live Sessions</h1>
         <div class="grid grid-cols-3 pt-4 relative">
-          <img src={DesignBook} alt='Designbook'></img> 
-          <img src={YouTube} alt='youtubelogo'  class="absolute top-14 left-20 "></img>
-          <img src={Notes} alt='Notes' class="pl-3"></img>
-          <img src={YouTube} alt='youtubelogo'  class="absolute top-14 left-80 ml-11"></img>
-          <img src={Groupwork} alt='groupdesign' class="pl-6"></img>
-          <img src={YouTube} alt='youtubelogo' class="absolute  top-14 left-96 ml-64"></img>
-          <p>Module 1: Introduction to<p>design</p></p> 
-          <p class="pl-3">Module 2: Sketching</p>
-          <p class="pl-6">Module 3: Ideation</p>
+          <img src={DesignBook} class=" h-32 w-64" alt='Designbook'></img> 
+          <img src={YouTube} alt='youtubelogo'  class="absolute top-14 left-24 w-16 h-14"></img>
+          <img src={Notes}  alt='Notes' class="pl-3 h-32 w-64"></img>
+          <img src={YouTube} alt='youtubelogo'  class="absolute top-14 left-80 ml-11 w-16 h-14"></img>
+          <img src={Groupwork} alt='groupdesign' class="pl-3 h-32 w-64"></img>
+          <img src={YouTube} alt='youtubelogo' class="absolute  top-14 left-96 ml-60 w-16 h-14"></img>
+          <p class="text-sm pt-2 font-bold">Module 1: Introduction to<p class="text-sm font-bold">design</p></p> 
+          <p class="pl-3 text-sm pt-2 font-bold">Module 2: Sketching</p>
+          <p class="pl-4 text-sm pt-2 font-bold">Module 3: Ideation</p>
         </div>
         <h1 class="text-2xl font-extrabold mt-8">Quiz</h1>
         <div class="pt-4 relative">
-        <img src={RectangleBox} alt='rectangle' class="pb-5"></img> 
-        <p class="absolute top-10 ml-4">Module 3: Responsive Design</p>
-        <a href='#' class="text-[#F64F59] absolute top-10 left-32 ml-96 underline">Start Quiz</a>
-        <img src={RectangleBox} alt='rectangle'></img>
-        <p class="absolute bottom-6 ml-4">Module 4: Responsive Design</p>
-        <a href='#' class="text-[#F64F59] absolute bottom-6 left-32 ml-96 underline">Start Quiz</a> 
+        <img src={RectangleBox} alt='rectangle' class="pb-5 w-4/5"></img> 
+        <p class="absolute top-9 font-bold ml-4 text-sm">Module 3: Responsive Design</p>
+        <a href='#' class="text-[#F64F59] absolute top-9 left-32 ml-96 underline text-sm">Start Quiz</a>
+        <img src={RectangleBox} alt='rectangle' class="w-4/5 h-16"></img>
+        <p class="absolute bottom-5 ml-4 text-sm font-bold">Module 4: Responsive Design</p>
+        <a href='#' class="text-[#F64F59] absolute bottom-5 left-32 ml-96 underline text-sm">Start Quiz</a> 
         </div>
         <h1 class="text-2xl font-extrabold mt-8">Completed Course</h1>
         <div class="relative">
-        <p class='absolute text-white pl-5 pt-3'>UI/UX Beginner Level</p>
+        <p class='absolute text-white pl-5 pt-3 text-sm'>UI/UX Beginner Level</p>
         <FontAwesomeIcon icon={faBars} class='absolute top-20 w-10 h-10  z-20 left-96 ml-36 text-white'/>
-        <img src={Overlay} alt='overlay' class='absolute left-96 ml-16'></img> 
-        <img src={Notes2}  alt='Notes' class="mt-3 mb-2"></img>
-        <p class="mb-6">1.3 UI/UX Beginner Level</p>
+        <img src={Overlay} alt='overlay' class='absolute left-96 ml-24 h-64 w-32'></img> 
+        <img src={Notes2} alt='Notes' class="mt-3 mb-2 h-64 w-4/5"></img>
+        <p class="mb-6 text-base font-bold">1.3 UI/UX Beginner Level</p>
         </div>
       </div>
     </div>
